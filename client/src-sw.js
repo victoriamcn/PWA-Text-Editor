5bdd0ae1538f0261a6a7bed3eb21ffc650755586
Victoria <vmcnorrill@gmail.com>
2023-06-23 16:18:10 -0400

    finished asset cache

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,8 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
+
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -27,4 +29,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  //define the callback function that will filter the requests we want to cache
+  ({request}) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+);
