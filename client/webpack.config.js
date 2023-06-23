fe063ecd5d7d140078660358e4389a8701f0ce32
Victoria <vmcnorrill@gmail.com>
2023-06-23 16:01:09 -0400

    wrote webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,21 +9,61 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 module.exports = () => {
   return {
     mode: 'development',
-    entry: {
+    entry: { // Entry point for files
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      // editor: './src/editor.js'
     },
-    output: {
+    output: { // Output for our bundles
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({ // Webpack plugin that generates our html file and injects our bundles. 
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+      new InjectManifest({ // Injects our custom service worker
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({ // Creates a manifest.json file.
+        fingerprints: false,
+        inject: true,
+        name: 'J.A.T.E',
+        short_name: 'JATE',
+        description: 'Create notes and code snippets with or without an internet connection.',
+        background_color: '#f5f5f5',
+        theme_color: '#31a9e1',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ],
+      }),
     ],
 
     module: {
-      rules: [
-        
+      rules: [ // CSS loaders
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude:/node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/present-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
