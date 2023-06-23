a82bdcab5ea76a0129d0e13e05c62c2ccfb6bb31
Victoria <vmcnorrill@gmail.com>
2023-06-23 17:11:08 -0400

    get and put req done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,10 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database (UPDATE)
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async ( content) => {
+  console.log('PUT to the database')
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('note', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('note', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('note');
+
+  // Use a method to update existing content
+  const request = store.put({ id: 1, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database (RETRIEVE)
-export const getDb = async () => {
+export const getDb = async (content) => {
   console.log('GET from the database')
 
   // Create a connection to the database database and version we want to use.
